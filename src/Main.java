3a850610712a15f6adae3138e1451aeeac0b8ac2
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-02-02 09:25:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(args[0]);
+
+        int a;
+        a = 1;
+        System.out.println("a = " + a);
+
+        char c = 49;
+        System.out.println("c = " + c);
+        System.out.println("c+1 = " + c + 1);
+        System.out.println("(c+1) = " + (c + 1));
+        System.out.println("(char)(c+1) = " + (char)(c + 1));
+        System.out.println("(int)c = " + (int) c);
+
+        char c1 = '1';
+        System.out.println("c1 = " + c1);
+        System.out.println("(int)c1 = " + (int) c1);
+    }
+}
