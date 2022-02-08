242d99f57892c3af714f8282821c92ce674cb7ff
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-02-08 09:38:29 +0200

    upd for test

--- src/io10/_00_Aleshchenko/Lab1.java
+++ src/io10/_00_Aleshchenko/Lab1.java
@@ -2,6 +2,6 @@ package io10._00_Aleshchenko;
 
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2");
     }
 }
