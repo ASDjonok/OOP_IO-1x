c61603d0afe3a901ec2bf431a92f60a1db8e79fc
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-02-10 10:20:02 +0200

    upd from second lection

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
-import myPackage.LogicOperations;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(args[0]);
-
-        int a;
-        a = 1;
+       /* System.out.println(args[0]);
+        System.out.println(args[3]);*/
+        int a = 1;
         System.out.println("a = " + a);
-
-        char c = 49;
+        char c = '1';
         System.out.println("c = " + c);
-        System.out.println("c+1 = " + c + 1);
+        System.out.println("c+1 = " + c + "1");
         System.out.println("(c+1) = " + (c + 1));
         System.out.println("(char)(c+1) = " + (char)(c + 1));
-        System.out.println("(int)c = " + (int) c);
-
+        System.out.println("(int)c = " + c);
         char c1 = '1';
         System.out.println("c1 = " + c1);
-        System.out.println("(int)c1 = " + (int) c1);
-
-        LogicOperations logicOperations;
+        System.out.println("(int)c1 = " + c1);
+        String s1 = "ASD";
+        System.out.println(s1);
+        String s2 = "ASD";
     }
 }
--- src/myPackage/LogicOperations.java
+++ src/myPackage/LogicOperations.java
@@ -2,6 +2,10 @@ package myPackage;
 
 public class LogicOperations {
     public static void main(String[] args) {
+        System.out.println(false && true);
+//        System.out.println(false & 12);
+        System.out.println(0 & 1);
+
         int a = 1;
         int b = 1;
         int n = 1;
