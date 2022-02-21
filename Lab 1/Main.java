c79e304237fb3901179ab6494e73387284759517
dfrnc-dev <login.dfrnc@gmail.com>
2022-02-21 22:56:38 +0200

    lab1+

--- Lab 1/Main.java
+++ Lab 1/Main.java
@@ -2,31 +2,31 @@
 public class Main {
     public static void main(String[] args) {
         
-        final int myNumber = 1310;  // 01  = +
-                                    // C   = 2
-                                    // 02  = *
-                                    // i,j = short
+        final int myNumber = 1310;  
+
+        // 01  = +
+        // C   = 2
+        // 02  = *
+        // i,j = short
 
         final int c = myNumber % 3;
-        final int a = 0;
+        final int a = 1;
         final int b = 0;
-        final int n = 1;
+        final int n = 5;
         final int m = 2;
 
         double sum = 0;
 
-        for (short i = a; i <= n; i++) {
-            double denominator = i + c;
-
-            if (denominator != 0) {
+        if ( a <= -c && -c <= n ) {
+            System.out.println("Error! Division by zero.");
+        } else {
+            for (short i = a; i <= n; i++) {
                 for (short j = b; j <= m; j++) {
-                    sum += i * j / denominator;
+                    sum += (i * j) / (i + c);
                 }
-            } else {
-                System.out.println("Error! Division by zero.");
             }
+    
+            System.out.printf("Your result: %f", sum);
         }
-
-        System.out.printf("Your result: %f", sum);
     }
 }
