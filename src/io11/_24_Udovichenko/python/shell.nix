7439f3132ee64612df4cf723f63ea60689bb31a3
viserctul <viserctul@pm.me>
2022-04-07 20:25:04 +0200

    Python nix files updated to use flakes

--- src/io11/_24_Udovichenko/python/shell.nix
+++ src/io11/_24_Udovichenko/python/shell.nix
@@ -1,20 +1,22 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{ nixpkgs ? builtins.fetchTarball "channel:nixpkgs-unstable"
+, system ? null
+}:
 
 let
-  pkgs = import (builtins.fetchTarball "channel:nixpkgs-unstable") {};
-  pythonEnv = pkgs.python310.withPackages (p: with p; [
-    pylint
-    ipython
-    notebook
+  pkgs = import nixpkgs ({
+  } // (
+    if (system != null)
+      then { inherit system; }
+      else {}
+  ));
 
-    numpy
-    matplotlib
-    pandas
+  pythonEnv = pkgs.python310.withPackages (p: with p; [
+    pylint black
+    ipython notebook
   ]);
 
 in pkgs.mkShell {
   buildInputs = [
-    pkgs.gcc
     pythonEnv
   ];
 
