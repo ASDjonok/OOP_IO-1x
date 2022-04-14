14b25dcdf14662620004487f75040a5f33767da0
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-04-14 10:21:09 +0300

    upd for lab3, lab2 + interesting test

--- src/Lab2.java
+++ src/Lab2.java
@@ -23,6 +23,7 @@ public class Lab2 {
         System.out.println(a.length);
 
         int[] a2 = {1, 2, 4};
+        System.out.println("a2 = " + a2);
 //test
         int[][] matrix2 = {
                 {1000, 2, 3, 5},
@@ -30,12 +31,19 @@ public class Lab2 {
         };
         System.out.println("matrix2.length = " + matrix2.length);
         System.out.println("Columns quantity = " + matrix2[0].length);
-        for (int i = 0; i < matrix2.length; i++) {
+        for (int[] ints : matrix2) {
+            for (int anInt : ints) {
+//                System.out.print(matrix2[i][j] + "\t");
+                System.out.printf("%5d", anInt);
+            }
+            System.out.println();
+        }
+        /*for (int i = 0; i < matrix2.length; i++) {
             for (int j = 0; j < matrix2[i].length; j++) {
 //                System.out.print(matrix2[i][j] + "\t");
                 System.out.printf("%5d", matrix2[i][j]);
             }
             System.out.println();
-        }
+        }*/
     }
 }
--- src/Lab3.java
+++ src/Lab3.java
@@ -1,22 +1,26 @@
 public class Lab3 {
     public static void main(String[] args) {
-        StringBuilder s = new StringBuilder("ASD");
+        /*StringBuilder s = new StringBuilder("ASD");
+        StringBuilder s2 = new StringBuilder("ASD");
         System.out.println(s);
         s.append("B");
         System.out.println(s);
         for (int i = 0; i < 100000; i++) {
             s.append("B");
         }
-        System.out.println(s);
+        System.out.println(s);*/
 
-        /*String s = "ASD";
+        String s = "ASD";
+        String s2 = "AS";
+        String s3 = s2 + "D";
+
         System.out.println(s);
         s += "B";
         System.out.println(s);
         for (int i = 0; i < 100000; i++) {
             s += "B";
         }
-        System.out.println(s);*/
+        System.out.println(s);
 
         /*for (int i = 0; i < 1000000; i++) {
             System.out.println("Done!");
