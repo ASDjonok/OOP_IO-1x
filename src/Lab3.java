9f79561a20b0c273f812c85c13e7995b3005b5cb
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-04-21 10:37:05 +0300

    upd for lab3

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,5 +1,12 @@
 public class Lab3 {
     public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer("ASD987");
+        System.out.println(stringBuffer);
+        stringBuffer.append("QWER");
+        System.out.println(stringBuffer);
+        stringBuffer.appendCodePoint(56);
+        System.out.println(stringBuffer);
+//        stringBuffer.
         /*StringBuilder s = new StringBuilder("ASD");
         StringBuilder s2 = new StringBuilder("ASD");
         System.out.println(s);
@@ -13,13 +20,13 @@ public class Lab3 {
         String s = "ASD";
         String s2 = "AS";
         String s3 = s2 + "D";
-
+        final String[] s1 = s.split("[SA]");
         System.out.println(s);
         s += "B";
         System.out.println(s);
-        for (int i = 0; i < 100000; i++) {
+        /*for (int i = 0; i < 100000; i++) {
             s += "B";
-        }
+        }*/
         System.out.println(s);
 
         /*for (int i = 0; i < 1000000; i++) {
