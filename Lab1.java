a641352137a9f3727e1c40c221720fe772fe44f6
ShynHlib <109368511+ShynHlib@users.noreply.github.com>
2022-07-15 20:38:46 +0300

    Lab1 created

---
+++ Lab1.java
@@ -0,0 +1,59 @@
+package com.company;
+import java.util.Scanner;
+
+public class Lab1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        final int C2 = 1329 % 2;/*1 => Операція О1 (-)*/
+        final int C5 = 1329 % 5;/*4 => Операція O2 (-)*/
+        final int C7 = 1329 % 7;/*6 => Тип індексів i та j (double)*/
+        final int C = 1329 % 3; /*0*/
+        int a;
+        int n;
+        int b;
+        int m;
+        double i;
+        double j;
+        double res = 0;
+
+        System.out.print("Type a: ");
+        checkInt(scanner);
+        a = scanner.nextInt();
+
+        System.out.print("Type n: ");
+        checkInt(scanner);
+        n = scanner.nextInt();
+
+        System.out.print("Type b: ");
+        checkInt(scanner);
+        b = scanner.nextInt();
+
+        System.out.print("Type m: ");
+        checkInt(scanner);
+        m = scanner.nextInt();
+
+        if (a == 0) {
+            System.out.println("Error! Division by zero! Try again");
+        } else if ((n < a) || (m < b)) {
+            System.out.println("Result: " + res);
+        } else {
+            for (i = a; i <= n; i++) {
+                for (j = b; b <= m; b++) {
+                    res += (i - j) / (i - C);
+                }
+            }
+            System.out.printf("Result: %.3f\n", res);
+        }
+    }
+
+    private static void checkInt(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please, type an integer");
+            scanner.next();
+        }
+    }
+}
+
