5c84b70f56557d5bed7e1aea3e4aa18dec73991e
Tim <enderko.times@gmail.com>
2022-07-29 16:45:08 +0300

    -

--- src/io14/Nezhurko/Lab_1/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("calculate_button").addEventListener("click", function() {
-    task = new Task(1418);
-    task.getOperations();
-    task.calculate();
-    task.outputResult();
-});
